dec44af13c938efa863ad118c106051d8dc58900
Imran01000 <imshaikh01000@gmail.com>
2020-03-29 12:43:07 +0530

    MODIFIED:Added student object

--- src/com/demopackage/FirstServlet.java
+++ src/com/demopackage/FirstServlet.java
@@ -14,12 +14,15 @@ public class FirstServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
 	{
+		Student student = new Student();
+		student.setName("Arsalan Shaikh");
 		String firstName = req.getParameter("fname");
 		String lastName = req.getParameter("lname");
 		String fullName = firstName + lastName;
 		
 		//PASSING AN OBJECT FROM SERVLET TO JSP.
 		req.setAttribute("name", fullName);
+		req.setAttribute("student", student);
 		RequestDispatcher dispatcher = req.getRequestDispatcher("test.jsp");
 		dispatcher.forward(req, resp);
 		
